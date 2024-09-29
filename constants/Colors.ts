35637be9a2a94232a17f37d1694449a110d40aaf
Akshay Gupta <iitb.akshay@gmail.com>
2024-09-29 21:08:55 +0530

    Install rnmapbox and change colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,16 +8,16 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#0B0C00',
+    background: '#FFFDFB',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#EEBC4B',
+    background: '#0B0C00',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
